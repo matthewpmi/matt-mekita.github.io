fef91092ea9a9f6641452e079e2ac1518bc18cc3
matthewpmi <mpmekita@gmail.com>
2022-09-28 01:14:25 +0000

    complete scratchpad day-2

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -13,7 +13,29 @@
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
 
-
+    // iterate through numbers 1-100
+    for (var i = 1; i <= 100; i++) {
+        // determine if number is not divisible by 3 or 5
+        if (i % 3 !== 0 && i % 5 !== 0) {
+            // log to console
+            console.log(i)
+        }
+        // else determine if number is divisible by 3 and 5
+        else if (i % 3 === 0 && i % 5 === 0) {
+            // log FizzBuzz to console
+            console.log('FizzBuzz');
+        }
+        // else determine if number is divisible by only 3
+        else if (i % 3 === 0) {
+            // log Fizz to console
+            console.log('Fizz');
+        }
+        // determine if number is divisible by only 5
+        else if (i % 5 === 0) {
+            // log Buzz to console
+            console.log('Buzz');
+        }
+    }
 
     // YOUR CODE GOES ABOVE HERE //
 }

289fac96cec39efd0f406a8020f55505a6a4be2a
matthewpmi <mpmekita@gmail.com>
2022-09-30 01:36:31 +0000

    complete scratchpad day-4

--- projects/scratch-pad/day-4/contact-list.js
+++ projects/scratch-pad/day-4/contact-list.js
@@ -35,7 +35,11 @@
 
 // YOUR CODE GOES BELOW HERE //
 function makeContact(id, nameFirst, nameLast) {
-
+    var obj = {};
+    obj.id = id;
+    obj.nameFirst = nameFirst;
+    obj.nameLast = nameLast;
+    return obj;
 } 
 
 
@@ -43,12 +47,49 @@ function makeContactList() {
     /*
      * You need something here to hold contacts. See length api for a hint:
      */
-    var contacts;
+    var contacts = [];
     
     return {
         // we implemented the length api for you //
         length: function() {
             return contacts.length;
+        },
+        // create addContact property with function as value that takes a parameter contact
+        addContact: function(contact){
+            // push parameter contact to contacts array
+            contacts.push(contact); 
+        },
+        // create findContact property with function as value that takes a parameter fullName
+        findContact: function(fullName){
+            // split fullName at space and assign it to array
+            var fullNameArray = fullName.split(' ');
+            // iterate over contacts array
+            for (var i = 0; i < contacts.length; i++){
+                // check if fullName corresponds to any nameFirst + nameLast in contact array
+                if (contacts[i].nameFirst === fullNameArray[0] && contacts[i].nameLast === fullNameArray[1]){
+                    // return applicable contacts
+                    return contacts[i];
+                }
+            } 
+        },
+        // create removeContact property with function as value that takes a parameter contact
+        removeContact: function(contact){
+            // remove contact from contacts array
+            contacts.splice(contact, 1);
+        },
+        // printAllContactNames property with function as value that takes no parameters
+        printAllContactNames: function(){
+            // create variable to store string
+            var string = '';
+            // iterate over contacts array
+            for (var i = 0; i < contacts.length; i++){
+                // concatenate each contact last and first name to string with spaces and new lines after each *except the last one
+                string += contacts[i].nameFirst + ' ' + contacts[i].nameLast + '\n';
+            }
+            // remove \n from end of string and assign it to a new variable
+            var result = string.slice(0, -1);
+            // return result
+            return result;
         }
     }
 }
---
+++ studies/control-flow.js
@@ -0,0 +1,16 @@
+/**
+ * VARIABLES:
+ * 
+ * 0: Variables are named containers for data in Javascript. To delacare a vaiable
+ * 
+ * 1: Declarations
+ * 
+ * 2: Var, let, const
+ * 
+ * 3: Hoisting:
+ * 
+ */
+
+// 1 Declaration
+var age; // variable age is declared but no assinged a value
+age = 35; // variable age is now assigned a value

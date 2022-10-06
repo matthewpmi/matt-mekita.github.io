6eca8f24e87f64f520ee3f9473f674a249e95bd0
matthewpmi <mpmekita@gmail.com>
2022-10-06 21:24:27 +0000

    add pseudocode to function-master and work on eloquent-javascript chap-2

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -2,23 +2,75 @@
 ////////////////////////////////////////////////////////////////////////////////
 // triangles ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-function triangles() {
-
+// create function triangles with 1 parameter num
+function triangles(num) {
+  // create empty string to hold #
+  var string = ''
+  // create for loop to iterate through numbers 1-num
+  for (var i = 1; i <= num; i++){
+    // concatenate # to string as loop iterates
+    string += '#'
+    // log to console string as loop iterates
+    console.log(string);
+  }
 }
 ////////////////////////////////////////////////////////////////////////////////
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
 function fizzBuzz() {
-  
+  // create for loop to iterate through numbers 1-15
+  for (var i = 1; i <= 15; i++){
+    // determine if i is divisible by 15
+    if (i % 15 === 0){
+      // log fizzbuzz
+      console.log('fizzbuzz');
+    }
+    // else determine if i is divisible by 5
+    else if (i % 5 === 0){
+      // log buzz
+      console.log('buzz');
+    }
+    // else determine if i is divisible by 3
+    else if (i % 3 === 0){
+      // log fizz
+      console.log('fizz');
+    }
+    // else log i
+    else {
+      console.log(i)
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function drawChessboard() {
+function drawChessboard(num) {
+  // create an empty string to hold output lines
+  var board = ''
+  // create a for loop to add new line at end of each row
+  for (var i = 1; i <= num; i++){
+    //create for loop to add characters to each row
+    for (var j = 1; j <= num; j++){
+      // use if statement to alternate ' ' and '#'
+      if ((i + j) % 2 !== 0) {
+        board += '#';
+      }
+      // else log ' '
+      else {
+        board += ' ';
+      }
+    }
+    // add new line to end of each line
+    board += '\n';
+    // remove new line at the end
+    // result = board.slice(0, -1)
+    
+  }
+  // console.log(result)
+  console.log(board);
 
 }
 
--- projects/function-master/function-master.js
+++ projects/function-master/function-master.js
@@ -1,50 +1,68 @@
 //////////////////////////////////////////////////////////////////////
 // Function 1 - Object Values ////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+// create function objectValues with object parameter
 function objectValues(object) {
+    // create empty array to hold object values
     var array = [];
+    // create for in loop to iterate through object keys
     for(var key in object){
+        // push object key values to array
        array.push(object[key]);
     }
+    // return array
     return array;
 } 
 
 //////////////////////////////////////////////////////////////////////
 // Function 2 - Keys to String ///////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+// create function keysToString with object as parameter
 function keysToString(object) {
+    // create variable with empty string as value
     var string = '';
+    // create for in loop to iterate through object keys
     for(var key in object){
+        // concatenate object keys to empty string with a space
         string += key + " ";
     }
+    // remove space at the end of the string
     var result = string.slice(0, -1)
+    // return string
     return result;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 3 - Values to String /////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+// create function valuesToString with object as parameter
 function valuesToString(object) {
+    // create variable with empty string as value
     var string = '';
+    // create for in loop to iterate through object
     for(var key in object){
+        // determine if the current object key's value is a string
         if(typeof object[key] === "string"){
+            // concatenate string with current object key's value with a space
             string += object[key] + " ";
         }
     }
+    // remove space at the end of the string
     var result = string.slice(0, -1)
+    // return string
     return result;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 4 - Array or Object //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+// create function arrayOrObject with colleciton as parameter
 function arrayOrObject(collection) {
+    // determine if its an array
     if(Array.isArray(collection)){
+        // return string array
         return 'array';
+        // else return string object
     } else {
         return 'object';
     }
@@ -53,29 +71,39 @@ function arrayOrObject(collection) {
 //////////////////////////////////////////////////////////////////////
 // Function 5 - Capitalize Word //////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+// create function capitalizeWord with string as paramter
 function capitalizeWord(string) {
+    // create firstChar variable with value of strings first character capitalized
     var firstChar = string.charAt(0).toUpperCase();
+    // update string to equal string without the first character
     string = string.slice(1, string.length);
+    // return the capitalized first character concatenated with the updated string
     return firstChar + string;
 }
 
 //////////////////////////////////////////////////////////////////////
 // Function 6 - Capitalize All Words /////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+// create function capitalizeAllWords with string as parameter
 function capitalizeAllWords(string) {
+    // create myArray variable with string split at spaces into array as value
     var myArray = string.split(" ");
+    // create myString variable with empty string as value
     var myString = '';
+    // create for loop to iterate through array
     for(var i = 0; i < myArray.length; i++){
-        var firstChar = myArray[i].charAt(0).toUpperCase();
-        var newWord = myArray[i].slice(1, myArray[i].length);
-        var finalWord = firstChar + newWord;
-        myString +=  " " + finalWord;         
+        // create variable finalWord with current string's first character capitalized concatenated with the string minus the first character
+        var finalWord = myArray[i].charAt(0).toUpperCase() + myArray[i].slice(1, myArray[i].length);
+        // concatenate myString with a space and finalWord
+        myString += " " + finalWord;         
     }
+    // remove space from front of myString
     var result = myString.slice(1, myString.length)
+    // return myString with all word's first letter capitalized
     return result;
 }
+// could have used finalWord.join(' ')
+// https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/#:~:text=In%20JavaScript%2C%20we%20have%20a,%22%3B%20publication%5B0%5D.
 
 //////////////////////////////////////////////////////////////////////
 // Function 7 - Welcome Message //////////////////////////////////////
